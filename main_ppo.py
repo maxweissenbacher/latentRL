d77f4fa72c7bce7933d9af05fc04a3af3fd83789
maxweissenbacher <max.weissenbacher@proton.me>
2024-04-10 19:56:12 +0100

    ppo config fix

--- main_ppo.py
+++ main_ppo.py
@@ -3,7 +3,7 @@ from ppo.training import main
 import hydra
 
 
-@hydra.main(version_base="1.2", config_path="sac/", config_name="config_sac")
+@hydra.main(version_base="1.2", config_path="ppo/", config_name="config_ppo")
 def train(cfg: "DictConfig"):
     main(cfg)
 
