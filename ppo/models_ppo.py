fixed normalisation; added CAE to SAC

ppo/models_ppo.py
@@ -13,24 +13,7 @@ from torchrl.envs import ExplorationType
 from torchrl.modules import MLP, ProbabilisticActor, TanhNormal, ValueOperator
 from autoencoder.visualize_cae_model import load_cae_model
 from pathlib import Path
-
-
-class CAEWrapper(torch.nn.Module):
-    def __init__(self, model, normalisation=1.):
-        super().__init__()
-        self.cae = model
-        self.normalisation_constant = normalisation
-
-    def forward(self, x):
-        batch_size = x.shape[:-1]
-        observation_size = x.shape[-1]
-        x = x.view(int(np.prod(batch_size)), 1, observation_size)
-        x = x/self.normalisation_constant
-        x = self.cae(x)
-        if isinstance(x, tuple):
-            x = x[1]
-        x = x.view(*batch_size, x.shape[-1])
-        return x
+from utils.wrappers import CAEWrapper
 
 
 # ====================================================================
@@ -44,10 +27,7 @@ def make_ppo_models(observation_spec, action_spec, path_to_model=None):
     if path_to_model:
         # Load trained CAE model
         modelpath = Path(path_to_model)
-        try:
-            cae = load_cae_model(modelpath)
-        except FileNotFoundError or RuntimeError:
-            raise RuntimeError(f"Model was not found at {modelpath}. Double check the model path.")
+        cae = load_cae_model(modelpath)
         encoder = cae.encoder
         cae = CAEWrapper(model=cae, normalisation=3.)
         encoder = CAEWrapper(model=encoder, normalisation=3.)
@@ -59,8 +39,6 @@ def make_ppo_models(observation_spec, action_spec, path_to_model=None):
             param.requires_grad = False
         # Set correct input size for MLP
         mlp_input_size = encoder.cae.enc_linear_dense.out_features
-        # Initialise a reshaper to handle reshaping of inputs for CAE
-
         print("Using CAE")
 
     # Define policy output distribution class
@@ -103,15 +81,17 @@ def make_ppo_models(observation_spec, action_spec, path_to_model=None):
         out_keys=["loc", "scale"],
     )
 
-    cae_module = TensorDictModule(
-        module=cae,
-        in_keys=["observation"],
-        out_keys=["cae_output"]
-    )
+    if path_to_model:
+        cae_module = TensorDictModule(
+            module=cae,
+            in_keys=["observation"],
+            out_keys=["cae_output"]
+        )
+        policy_module = TensorDictSequential(policy_module, cae_module)
 
     # Add probabilistic sampling of the actions
     policy_module = ProbabilisticActor(
-        TensorDictSequential(policy_module, cae_module),
+        policy_module,
         in_keys=["loc", "scale"],
         spec=CompositeSpec(action=action_spec),
         distribution_class=distribution_class,

sac/agents_sac.py
@@ -6,6 +6,7 @@
 import tempfile
 from contextlib import nullcontext
 import torch
+from pathlib import Path
 import numpy as np
 from tensordict.nn import InteractionType, TensorDictModule, TensorDictSequential
 from tensordict.nn.distributions import NormalParamExtractor
@@ -18,6 +19,8 @@ from torchrl.modules import MLP, ProbabilisticActor, ValueOperator
 from torchrl.modules.distributions import TanhNormal
 from torchrl.objectives import SoftUpdate, SACLoss
 from utils.rng import env_seed
+from utils.wrappers import CAEWrapper
+from autoencoder.visualize_cae_model import load_cae_model
 import wandb
 
 
@@ -80,6 +83,7 @@ def make_replay_buffer(cfg, prefetch=3):
 # Model
 # -----
 def make_sac_agent(cfg, train_env, eval_env):
+    path_to_model = cfg.env.path_to_cae_model
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     # Define Actor Network
     in_keys_actor = ["observation"]
@@ -87,6 +91,38 @@ def make_sac_agent(cfg, train_env, eval_env):
     action_spec = train_env.action_spec
     if train_env.batch_size:
         action_spec = action_spec[(0,) * len(train_env.batch_size)]
+
+    if path_to_model:
+        # Load trained CAE model
+        modelpath = Path(path_to_model)
+        cae = load_cae_model(modelpath)
+        encoder = cae.encoder
+        cae = CAEWrapper(model=cae, normalisation=3.)
+        encoder = CAEWrapper(model=encoder, normalisation=3.)
+        print(f"Using a normalisation of {cae.normalisation_constant}. CHECK that this is correct for the model used!")
+
+        # Freeze parameters
+        for param in encoder.parameters():
+            param.requires_grad = False
+        for param in cae.parameters():
+            param.requires_grad = False
+
+        # Wrap the CAE into a TensorDictModule for composition
+        encoder_module = TensorDictModule(
+            encoder,
+            in_keys=in_keys_actor,
+            out_keys=["latent_state"],
+        )
+        cae_module = TensorDictModule(
+            cae,
+            in_keys=in_keys_actor,
+            out_keys=["cae_output"],
+        )
+
+        # Redefine the input keys for the actor and value MLP
+        in_keys_actor = encoder_module.out_keys
+
+        print("Using CAE")
 
     activation = nn.ReLU
     actor_net = MLP(
@@ -108,7 +144,10 @@ def make_sac_agent(cfg, train_env, eval_env):
         out_keys=["loc", "scale"],
     )
 
-    actor_module = TensorDictSequential(actor_net, actor_extractor)
+    if path_to_model:
+        actor_module = TensorDictSequential(encoder_module, actor_net, actor_extractor, cae_module)
+    else:
+        actor_module = TensorDictSequential(actor_net, actor_extractor)
 
     actor = ProbabilisticActor(
         spec=action_spec,
@@ -133,10 +172,17 @@ def make_sac_agent(cfg, train_env, eval_env):
     qvalue_net = MLP(
         **qvalue_net_kwargs,
     )
-    critic = ValueOperator(
-        in_keys=["action"] + in_keys_actor,
-        module=qvalue_net,
-    )
+    if path_to_model:
+        qvalue_module = ValueOperator(
+            in_keys=["action"] + encoder_module.out_keys,
+            module=qvalue_net,
+        )
+        critic = TensorDictSequential(encoder_module, qvalue_module)
+    else:
+        critic = ValueOperator(
+            in_keys=["action"] + in_keys_actor,
+            module=qvalue_net,
+        )
 
     model = nn.ModuleList([actor, critic]).to(device)
 
@@ -175,14 +221,17 @@ def make_sac_optimizer(cfg, loss_module):
     critic_params = list(loss_module.qvalue_network_params.flatten_keys().values())
     actor_params = list(loss_module.actor_network_params.flatten_keys().values())
 
+    trainable_actor_params = filter(lambda p: p.requires_grad, actor_params)
+    trainable_critic_params = filter(lambda p: p.requires_grad, critic_params)
+
     optimizer_actor = optim.Adam(
-        actor_params,
+        trainable_actor_params,
         lr=cfg.optim.lr,
         weight_decay=cfg.optim.weight_decay,
         eps=cfg.optim.adam_eps,
     )
     optimizer_critic = optim.Adam(
-        critic_params,
+        trainable_critic_params,
         lr=cfg.optim.lr,
         weight_decay=cfg.optim.weight_decay,
         eps=cfg.optim.adam_eps,

sac/training.py
@@ -14,7 +14,7 @@ from tensordict import TensorDict
 from torchrl.envs.utils import ExplorationType, set_exploration_type
 import wandb
 from torchrl.record.loggers import generate_exp_name
-from agents_sac import (
+from sac.agents_sac import (
     log_metrics_offline,
     log_metrics_wandb,
     make_collector,
@@ -27,7 +27,7 @@ from utils.rng import env_seed
 from env.ks_env_utils import make_parallel_ks_env, make_ks_eval_env
 
 
-@hydra.main(version_base="1.2", config_path="", config_name="config_sac")
+# @hydra.main(version_base="1.2", config_path="", config_name="config_sac")
 def main(cfg: "DictConfig"):  # noqa: F821
 
     LOGGING_TO_CONSOLE = False
@@ -128,9 +128,7 @@ def main(cfg: "DictConfig"):  # noqa: F821
 
                 # Update actor
                 optimizer_actor.zero_grad()
-                # For LSTM architecture, need retain_graph=True
-                retain_graph = cfg.network.architecture == 'lstm' or cfg.network.architecture == 'buffer_lstm'
-                actor_loss.backward(retain_graph=retain_graph)
+                actor_loss.backward(retain_graph=False)
                 optimizer_actor.step()
 
                 # Update critic
@@ -197,6 +195,17 @@ def main(cfg: "DictConfig"):  # noqa: F821
                 # Compute mean and std of actuation
                 mean_actuation = torch.linalg.norm(eval_rollout["action"], dim=-1).mean(-1).mean().item()
                 std_actuation = torch.linalg.norm(eval_rollout["action"], dim=-1).std(-1).mean().item()
+                # Compute length of rollout
+                terminated = eval_rollout["terminated"].nonzero()
+                if terminated.nelement() > 0:
+                    rollout_episode_length = terminated[0][0].item()
+                else:
+                    rollout_episode_length = cfg.logger.test_episode_length
+                # Compute CAE accuracy during evaluation rollout
+                if cfg.env.path_to_cae_model:
+                    cae_output = eval_rollout["cae_output"].detach().cpu().numpy()
+                    u = eval_rollout["u"].detach().cpu().numpy()
+                    cae_rel_error = np.linalg.norm(cae_output - u) / np.linalg.norm(u)
 
             log_info.update(
                 {
@@ -205,8 +214,15 @@ def main(cfg: "DictConfig"):  # noqa: F821
                     "eval/mean_actuation": mean_actuation,
                     "eval/std_actuation": std_actuation,
                     "eval/time": eval_time,
+                    "eval/episode_length": rollout_episode_length,
                 }
             )
+            if cfg.env.path_to_cae_model:
+                log_info.update(
+                    {
+                        "eval/cae_relative_L2_error": cae_rel_error,
+                    }
+                )
 
         wandb.log(data=log_info, step=collected_frames)
         sampling_start = time.time()