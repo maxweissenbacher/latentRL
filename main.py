867a5946732d11c7070a5be9963f4e27acffaa62
maxweissenbacher <max.weissenbacher@proton.me>
2024-04-10 14:48:37 +0200

    small chagne to main

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from ppo.training import main
 from hydra import compose, initialize
 
 if __name__ == '__main__':
-    initialize(config_path="./ppo/", job_name="test_app", version_base="1.2")
-    cfg = compose(config_name="config_ppo")
-    main(cfg)
+    algorithm = 'PPO'  # or 'SAC'
+
+    print(f"Executing training with {algorithm}.")
+
+    if algorithm == 'PPO':
+        initialize(config_path="./ppo/", job_name="test_app", version_base="1.2")
+        cfg = compose(config_name="config_ppo")
+        from ppo.training import main
+        main(cfg)
+
