81a3a49e33746b88c777dc16d7f00626c2a2eab0
maxweissenbacher <max.weissenbacher@proton.me>
2024-04-10 19:40:03 +0100

    Fixed hydra for PPO

--- main_ppo.py
+++ main_ppo.py
@@ -1,8 +1,13 @@
 from hydra import compose, initialize
+from ppo.training import main
+import hydra
+
+
+@hydra.main(version_base="1.2", config_path="sac/", config_name="config_sac")
+def train(cfg: "DictConfig"):
+    main(cfg)
+
 
 if __name__ == '__main__':
-    initialize(config_path="./ppo/", job_name="ppo", version_base="1.2")
-    cfg = compose(config_name="config_ppo")
-    from ppo.training import main
-    main(cfg)
+    train()
 
--- main_sac.py
+++ main_sac.py
@@ -2,6 +2,7 @@ from hydra import compose, initialize
 from sac.training import main
 import hydra
 
+
 @hydra.main(version_base="1.2", config_path="sac/", config_name="config_sac")
 def train(cfg: "DictConfig"):
     main(cfg)
