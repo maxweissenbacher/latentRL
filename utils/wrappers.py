fixed normalisation in CAE wrapper

utils/wrappers.py
@@ -3,10 +3,11 @@ import numpy as np
 
 
 class CAEWrapper(torch.nn.Module):
-    def __init__(self, model, normalisation=1.):
+    def __init__(self, model, normalisation=1., un_normalise=False):
         super().__init__()
         self.cae = model
         self.normalisation_constant = normalisation
+        self.un_normalise = un_normalise
 
     def forward(self, x):
         batch_size = x.shape[:-1]
@@ -17,5 +18,7 @@ class CAEWrapper(torch.nn.Module):
         if isinstance(x, tuple):
             x = x[1]
         x = x.view(*batch_size, x.shape[-1])
+        if self.un_normalise:
+            x = x*self.normalisation_constant
         return x
 