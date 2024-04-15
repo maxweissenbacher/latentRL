b4e6750b9219f54c620211a0c3b0c862dbb9869d
maxweissenbacher <max.weissenbacher@proton.me>
2024-04-15 17:55:52 +0100

    bug fix: env was not resetting for SAC

--- sac/agents_sac.py
+++ sac/agents_sac.py
@@ -36,7 +36,7 @@ def make_collector(cfg, train_env, actor_model_explore):
         frames_per_batch=cfg.collector.frames_per_batch // cfg.env.frame_skip,
         total_frames=cfg.collector.total_frames // cfg.env.frame_skip,
         device=cfg.collector.device,
-        # max_frames_per_traj=cfg.env.max_episode_steps_train // cfg.env.frame_skip,
+        max_frames_per_traj=cfg.collector.max_episode_length // cfg.env.frame_skip,
     )
     collector.set_seed(env_seed(cfg))
     return collector
