e89d7a5f95697f1a2f35c5dd305aa7f702c4b998
maxweissenbacher <max.weissenbacher@proton.me>
2024-05-17 17:46:23 +0100

    removed unnecessary stuff from cae wrapper

--- env/cylinder_flow_env.py
+++ env/cylinder_flow_env.py
@@ -7,17 +7,22 @@ from Cylinder_Env.simulation_base.env_raw_bp import resume_env
 from torchrl.envs.utils import check_env_specs
 
 
-def make_cylinder_torchrl_env(device):
+def make_cylinder_env(device):
     from torchrl.envs.libs.gym import GymWrapper
     # Create the 2D cylinder Gym environment here
     env = resume_env(plot=False, single_run=True, horizon=400, dump_vtu=100, n_env=99)
     env = GymWrapper(env, device=device)
     return env
 
+def make_parallel_cylinder_env(cfg):
+    make_env_fn = EnvCreator(lambda: make_ks_env(cfg))
+    env = ParallelEnv(cfg.env.num_envs, make_env_fn)
+    return env
+
 
 if __name__ == '__main__':
     # Create torchrl env
-    env = make_cylinder_torchrl_env(device='cpu')
+    env = make_cylinder_env(device='cpu')
 
     # Test reset
     print(env.reset())
--- utils/wrappers.py
+++ utils/wrappers.py
@@ -3,22 +3,17 @@ import numpy as np
 
 
 class CAEWrapper(torch.nn.Module):
-    def __init__(self, model, normalisation=1., un_normalise=False):
+    def __init__(self, model):
         super().__init__()
         self.cae = model
-        self.normalisation_constant = normalisation
-        self.un_normalise = un_normalise
 
     def forward(self, x):
         batch_size = x.shape[:-1]
         observation_size = x.shape[-1]
         x = x.view(int(np.prod(batch_size)), 1, observation_size)
-        x = x/self.normalisation_constant
         x = self.cae.symp_forward(x)
         if isinstance(x, tuple):
             x = x[1]
         x = x.view(*batch_size, x.shape[-1])
-        if self.un_normalise:
-            x = x*self.normalisation_constant
         return x
 
